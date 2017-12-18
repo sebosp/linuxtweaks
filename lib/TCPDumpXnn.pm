fdfd69b787747a785e190ad335cc0d9e9693498a
Seb Ospina <kraige@gmail.com>
2017-12-18 22:14:02 +0100

    Removing IPv4 Header

--- lib/TCPDumpXnn.pm
+++ lib/TCPDumpXnn.pm
@@ -6,7 +6,7 @@ use Sub::Identify;
 use namespace::autoclean;
 
 has inputFile   => (is => 'ro', isa => 'Str',     default => '/var/log/tcpdump.log');
-has filterPort => (is => 'ro', isa => 'Num',     default => 0);
+has filterPort  => (is => 'ro', isa => 'Num',     default => 0);
 has packets     => (is => 'rw', isa => 'HashRef', default => sub{{}});
 has headerRegEx => (is => 'ro', isa => 'RegexpRef',
 	default => sub{qr/^([0-9:.]{15}) IP ([0-9.]+)\.([0-9]+) > ([0-9.]+)\.([0-9.]+): Flags .*/}
@@ -22,6 +22,65 @@ sub checkFile{
 	}
 	return 1;
 }
+
+sub isIPv4{
+	my ($self,$packetData) = @_;
+	print "Got data: $packetData\n";
+	my ($version) = split(//,$packetData);
+	if($version eq "4"){
+		return 1;
+	}
+	return 0;
+}
+
+sub removeIPv4Header{
+	my ($self,$packetKey) = @_;
+	my %packet = %{ $self->packets() };
+	return 0 unless($self->isIPv4($packet{$packetKey}{payload}[0]));
+	my ($version,$headerSize) = split(//,$packet{$packetKey}{payload}[0]);
+	# headerSize is how many 32-bit words is header
+	# Transformed into bytes n*32/8 => n*4
+	$headerSize *= 4;
+	if($headerSize > scalar(@{$packet{$packetKey}{payload}})){
+		# Invalid packet size.
+		return 0;
+	}
+	splice(@{$packet{$packetKey}{payload}},0,$headerSize);
+	$packet{$packetKey}{_hasIPv4Header}=0;
+	$self->packets(\%packet);
+	return 1;
+}
+
+sub removeTCPHeader{
+	my ($self,$packetKey) = @_;
+	my %packet = %{ $self->packets() };
+	if(!exists($packet{$packetKey}{_hasIPv4Header}) || $packet{$packetKey}{_hasIPv4Header} == 1){
+		return -1 unless($self->removeIPv4Header($packetKey));
+	}
+	# Byte Pos | Packet Payload fields:
+	# 0        | [Source port]	[Destination port]
+	# 4        | [Sequence number]x2
+	# 8        | [Acknowledgment number (if ACK set)]x2
+	# 12       | [Data offset(4 bits) ... reserved ]
+	my $dataOffsetPosition = 12;
+	if($dataOffsetPosition > scalar(@{$packet{$packetKey}{payload}})){
+		return 0;
+	}
+	my ($dataOffset) = split(//,$packet{$packetKey}{payload}[$dataOffsetPosition]);
+	# dataOffset is how many 32-bit words is TCP header
+	# Transformed into bytes n*32/8 => n*4
+	$dataOffset *= 4;
+	if($dataOffset > scalar(@{$packet{$packetKey}{payload}})){
+		# invalid packet size
+		return 0;
+	}
+	splice(@{$packet{$packetKey}{payload}},0,$dataOffset);
+	$packet{$packetKey}{_hasTCPHeader}=0;
+	$self->packets(\%packet);
+	return 1;
+	
+}
+
 sub readFile{
 	my ($self) = @_;
 	open(my $fh,"<",$self->inputFile);
@@ -48,6 +107,15 @@ sub readFile{
 	}
 	close($fh);
 	$self->packets(\%packet);
+}
+
+sub cleanHeaders{
+	my($self) = @_;
+	my %packet = %{ $self->packets() };
+	foreach my $packetTime(keys(%packet)){
+		next unless ($self->removeIPv4Header($packetTime));
+		next unless ($self->removeTCPHeader($packetTime));
+	}
 }
 
 __PACKAGE__->meta->make_immutable();
--- t/tcpdumpXnn.t
+++ t/tcpdumpXnn.t
@@ -13,7 +13,14 @@ ok(my $fileExists = TCPDumpXnn->new(
 is($fileExists->checkFile(),1,"File exists recognized");
 ok($fileExists->readFile(),"File read properly");
 ok(exists($fileExists->packets->{"17:50:04.932359"}), "Date key exists");
-is($fileExists->packets->{"17:50:04.932359"}->{payload}->[0],"45", "Data header first chars");
-is($fileExists->packets->{"17:50:04.932359"}->{payload}->[-1],"0a", "Data header last chars");
+is($fileExists->packets->{"17:50:04.932359"}->{payload}->[0],"45", "IP Packet header first byte");
+is($fileExists->packets->{"17:50:04.932359"}->{payload}->[-1],"0a", "IP Packet header last byte");
+is($fileExists->removeIPv4Header("17:50:04.932359"), 1, "Removing IPv4 Header");
+is($fileExists->packets->{"17:50:04.932359"}->{payload}->[0],"fa", "TCP Packet header first byte");
+my %packets = %{$fileExists->packets()};
+push(@{$packets{nonIPv4Header}{payload}},(66,00));
+is($fileExists->isIPv4("66"), 0, "Invalid IPv4 header");
+#is($fileExists->removeIPv4Header("nonIPv4Header"), 0, "Not removing invalid IPv4 header");
+#push(@{$packets{invalidIPv4HeaderSize}{payload}},"45");
 		#17:50:04.932359 IP 223.105.4.248.64114 > 106.186.123.201.80: Flags [P.], seq 1:261, ack 1, win 115, options [nop,nop,TS val 2848484540 ecr 1760720924], length 260: HTTP: GET http://112.124.116.170:83/index.php HTTP/1.1
 done_testing();
